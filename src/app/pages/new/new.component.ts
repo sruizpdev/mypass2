a6a717dbc7d85c1c053f0f36ca96c8c7d0d9297b
Sergio Ruiz <sergiotto@outlook.com>
2023-11-23 14:21:31 +0100

    feat: add form to new component

--- src/app/pages/new/new.component.ts
+++ src/app/pages/new/new.component.ts
@@ -1,15 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './new.component.html',
   styleUrl: './new.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NewComponent { }
+export class NewComponent {
+  newForm = new FormGroup({
+    site: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+  onSubmit() {
+    // this.userService.login(this.loginForm.value);
+    console.warn(this.newForm.value);
+  }
+  clearForm(): void {
+     this.newForm.patchValue({ site:'',username: '', password: '' });
+    console.log('borrado');
+  }
+}
