41c5979dc85b00cf99e2f55db46924f87a2eaff4
Sergio Ruiz <sergiotto@outlook.com>
2023-11-24 14:00:02 +0100

    feat: add queryparams to edit component

--- src/app/pages/edit/edit.component.ts
+++ src/app/pages/edit/edit.component.ts
@@ -1,16 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { NavbarComponent } from '../../shared/navbar/navbar.component';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EditComponent { }
+export class EditComponent {
+  newForm = new FormGroup({
+    site: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor(private router: ActivatedRoute) {
+    
+    //todo: hay que darle un tipo a val , por ejemplo usando una interfaz 
+
+    this.router.queryParams.subscribe((val: any) => {
+      console.log(val);
+      this.newForm.patchValue({
+        site: val.site,
+        username: val.username,
+        password: val.password,
+      });
+    });
+  }
+  onSubmit() {
+    // this.userService.login(this.loginForm.value);
+    console.warn(this.newForm.value);
+  }
+  clearForm(): void {
+    this.newForm.patchValue({ site: '', username: '', password: '' });
+    console.log('borrado');
+  }
+}
