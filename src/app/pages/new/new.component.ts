9b8e1e9b9d1b19b5cba9bc4a482962f66e7185b7
Sergio Ruiz <sergiotto@outlook.com>
2023-11-27 13:38:13 +0100

    feat: add new field to new component

--- src/app/pages/new/new.component.ts
+++ src/app/pages/new/new.component.ts
@@ -12,16 +12,26 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class NewComponent {
   newForm = new FormGroup({
+    owner: new FormControl(''),
     site: new FormControl(''),
     username: new FormControl(''),
     password: new FormControl(''),
+    notes: new FormControl(''),
   });
   onSubmit() {
-    // this.userService.login(this.loginForm.value);
+    
     console.warn(this.newForm.value);
   }
+
+  
   clearForm(): void {
-     this.newForm.patchValue({ site:'',username: '', password: '' });
+    this.newForm.patchValue({
+      owner: '',
+      site: '',
+      username: '',
+      password: '',
+      notes: '',
+    });
     console.log('borrado');
   }
 }
