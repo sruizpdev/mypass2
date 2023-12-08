79f868d6c3b693142ef8d2df8c344d31d306ecf3
Sergio Ruiz <sergiotto@outlook.com>
2023-12-08 13:58:23 +0100

    feat:add quote

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,7 @@
-import { collectionData } from '@angular/fire/firestore';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { ChangeDetectionStrategy, Component} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { UserService } from '../../user.service';
-import { User } from '../../user';
 import { Observable } from 'rxjs';
 
 @Component({
