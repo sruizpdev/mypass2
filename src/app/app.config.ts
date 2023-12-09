1c380a5c98f358e8810e487f49a8b060e9071b16
Sergio Ruiz <sergiotto@outlook.com>
2023-12-09 18:05:57 +0100

    build: add pwa support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,19 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { enviroment } from './enviroments/enviroment';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(enviroment.firebaseConfig)),
-      provideAuth(() => getAuth()),
-      provideFirestore(() => getFirestore())
-    ),
-  ],
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(enviroment.firebaseConfig)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
